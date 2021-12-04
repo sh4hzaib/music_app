6d5d3beb7685a5d09349c44832333705759174b9
Muhammad Shahzaib Ishaq <shexbi119@gmail.com>
2021-12-04 14:20:32 +0500

    implemented audio linking, songs are loaded, and each mp3 is playing

--- src/screens/Tracks.js
+++ src/screens/Tracks.js
@@ -1,12 +1,34 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import { View, Text, TouchableOpacity } from "react-native";
 
+import { Audio } from "expo-av";
+
 const Tracks = ({ route }) => {
-  const { tracks } = route.params;
-  useEffect(() => {
-    console.log(tracks);
-    return () => {};
+  const [sound, setSound] = useState();
+
+  const handleSetTrack = useCallback(async mp3_link => {
+    console.log(mp3_link);
+    const { sound } = await Audio.Sound.createAsync({
+      uri: mp3_link
+    });
+    setSound(sound);
+
+    console.log("Playing Sound");
+    await sound.playAsync();
   }, []);
+  const { tracks } = route.params;
+  useEffect(
+    () => {
+      //   console.log(tracks);
+      return sound
+        ? () => {
+            console.log("Unloading Sound");
+            sound.unloadAsync();
+          }
+        : undefined;
+    },
+    [sound]
+  );
   return (
     <View>
       <Text>TRACKS</Text>
@@ -16,6 +38,7 @@ const Tracks = ({ route }) => {
           <TouchableOpacity
             key={track.id}
             onPress={() => {
+              handleSetTrack(track.media.mp3.url);
               //   navigation.navigate("Tracks", { tracks: track.tracks });
             }}
           >
