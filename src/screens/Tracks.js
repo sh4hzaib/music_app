f13513ddee78eb3acdfffc789c9a46feb7ea5ace
Muhammad Shahzaib Ishaq <shexbi119@gmail.com>
2021-12-05 07:54:03 +0500

    songs basic ui completed

--- src/screens/Tracks.js
+++ src/screens/Tracks.js
@@ -1,19 +1,29 @@
 import React, { useEffect, useState, useCallback } from "react";
-import { View, Text, TouchableOpacity, Button } from "react-native";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Button,
+  StyleSheet,
+  Image
+} from "react-native";
 
 import { Audio } from "expo-av";
+import Icon from "react-native-vector-icons/Entypo";
+import { symbolicateLogNow } from "react-native/Libraries/LogBox/Data/LogBoxData";
 
 const Tracks = ({ route }) => {
   const [sound, setSound] = useState(new Audio.Sound());
   const [playing, setplaying] = useState(false);
+  const [c_url, setc_url] = useState();
 
   const handleSetTrack = useCallback(
     async mp3_link => {
-      sound._loaded
-        ? sound.replayAsync()
-        : await sound.loadAsync({
-            uri: mp3_link
-          });
+      playing ? handleExit() : null;
+      sound._loaded ? sound.replayAsync() : console.log(mp3_link);
+      await sound.loadAsync({
+        uri: mp3_link
+      });
       console.log("3Playing Sound");
       setplaying(true);
       await sound.playAsync();
@@ -41,20 +51,20 @@ const Tracks = ({ route }) => {
   );
   return (
     <View>
-      <Text>TRACKS</Text>
-
-      {tracks.map(track => {
+      {tracks.map(item => {
         return (
           <TouchableOpacity
-            key={track.id}
             onPress={() => {
-              handleSetTrack(track.media.mp3.url);
-              //   navigation.navigate("Tracks", { tracks: track.tracks });
+              handleSetTrack(item.media.mp3.url);
             }}
+            style={[styles.item]}
           >
-            <Text>
-              {track.title}
-            </Text>
+            <View style={styles.titleView}>
+              <Icon name="music" size={20} color="white" />
+              <Text style={[styles.title]}>
+                {item.title}
+              </Text>
+            </View>
           </TouchableOpacity>
         );
       })}
@@ -71,5 +81,32 @@ const Tracks = ({ route }) => {
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  item: {
+    // // height: 140,
+    // marginVertical: 10,
+    // marginHorizontal: 10
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: "500",
+    color: "white"
+  },
+  titleView: {
+    backgroundColor: "blue",
+    padding: 8,
+    borderBottomEndRadius: 10,
+    shadowOpacity: 1,
+    flexDirection: "row"
+  },
+  image: {
+    flex: 1,
+    shadowOpacity: 0.5
+  }
+});
 
 export default Tracks;
